9e3e7b27b5cfe7ca50479fc3d8b22150b0a16850
Ihar Bandarenka <ihar_bandarenka@goodyear.com>
2023-05-29 08:54:53 +0300

    Task 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using WordsGame;
+
+const int Min = 8;
+const int Max = 30;
+
+
+Console.WriteLine($"Enter the word of {Min}-{Max} letters:");
+
+string allowedSymbols = Console.ReadLine();
+
+while (!(Utils.isInitialWordValid(allowedSymbols, Min, Max)))
+{
+    Console.WriteLine($"Invalid input. Please enter a word of {Min}-{Max} symbols length containing only letters.");
+    allowedSymbols = Console.ReadLine();
+}
+
+bool shouldContinue;
+do
+{
+    shouldContinue = Utils.PlayRound(allowedSymbols);
+}
+while (shouldContinue);
+
+Console.ReadKey();
---
+++ Utils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsGame
+{
+    internal class Utils
+    {
+        public static bool isInitialWordValid(string? input, int min, int max)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            if (input.Any(c => !Char.IsLetter(c)))
+            {
+                return false;
+            }
+
+            if (input.Length < min || input.Length > max)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+
+        public static bool IsWordValid(string allowedSymbols, string? input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            if (input.Any(c => !allowedSymbols.Contains(c)))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+        public static bool PlayRound(string? allowedSymbols)
+        {
+            Func<string, bool> MakeTurn = user =>
+            {
+                Console.WriteLine($"\n{user}'s word:");
+                string? input = Console.ReadLine();
+                if (IsWordValid(allowedSymbols, input))
+                {
+                    return true;
+                }
+                Console.WriteLine($"{user} has lost the game!");
+                return false;
+                
+            };
+
+            return MakeTurn("User 1") && MakeTurn("User 2");
+
+        }
+
+    }
+}
